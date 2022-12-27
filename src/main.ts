2403fdbe7f4eb23fb3196b3504345ff59ee14a46
Daniel Romero <daniel.romero@manzanaverde.la>
2022-12-26 23:53:55 -0500

    feat: se agregan ejemplos de estructuras de datos

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import LinkedList from "./structures/01-lists";
+import { Queue } from "./structures/02-queues";
+
+const list = new LinkedList();
+list.addFirst('3')
+list.addLast('0')
+list.addLast('2')
+console.log(list);
